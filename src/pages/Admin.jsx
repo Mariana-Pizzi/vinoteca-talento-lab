b5bad641dd981224d1fbfd4c5e6fb6fedce3ee54
Mariana <mpizzi@fi.uba.ar>
2025-10-17 14:31:13 -0300

    implemento Admin

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Panel de Administración</h1>
+      <p>Aquí podrás gestionar los vinos del catálogo.</p>
+    </div>
+  );
+}
