86d62b620ea55977810fd04a495a91c72f38fa17
Mariana <mpizzi@fi.uba.ar>
2025-10-17 14:30:39 -0300

    implemento page/Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '30px' }}>
+      <h1>Bienvenido a Vinoteca TalentoLab 🍷</h1>
+      <p>Descubrí los mejores vinos seleccionados para vos.</p>
+    </div>
+  );
+}
