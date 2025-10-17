57e2f55eb7d91712ebe6e0c225c0197802f3f56f
Mariana <mpizzi@fi.uba.ar>
2025-10-17 14:29:39 -0300

    implemento carritoContext

---
+++ src/context/CarritoContext.jsx
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from 'react';
+
+export const CarritoContext = createContext();
+
+export function CarritoProvider({ children }) {
+  const [carrito, setCarrito] = useState([]);
+
+  const agregarAlCarrito = (producto) => setCarrito([...carrito, producto]);
+  const vaciarCarrito = () => setCarrito([]);
+
+  return (
+    <CarritoContext.Provider value={{ carrito, agregarAlCarrito, vaciarCarrito }}>
+      {children}
+    </CarritoContext.Provider>
+  );
+}
